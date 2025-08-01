29bfb36b5c164190f14f16a5e6ac5c3881c64343
khj0105 <khj0001052@gmail.com>
2025-08-01 23:59:54 +0900

    refactor(enums): Rename AdminChangeStatus to ChangeStatus

--- backend/src/main/java/com/example/high_school_course_registration/common/enums/ChangeStatus.java
+++ backend/src/main/java/com/example/high_school_course_registration/common/enums/ChangeStatus.java
@@ -3,7 +3,7 @@ package com.example.high_school_course_registration.common.enums;
 import lombok.Getter;
 
 @Getter
-public enum AdminChangeStatus {
+public enum ChangeStatus {
     PENDING,
     APPROVED,
     REJECTED
